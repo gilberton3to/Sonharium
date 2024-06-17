20f0a988ddd2a5f217c58be82223cf0d2bcd4234
Gilberto Neto <gilberytorid@gmail.com>
2024-06-17 13:34:32 -0300

    Initial Commit

---
+++ Sonharium/SonhariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonhariumApp.swift
+//  Sonharium
+//
+//  Created by Gilberto Neto on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SonhariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
